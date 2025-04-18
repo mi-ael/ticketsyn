9cf06a5f9551a5e9e49ad258e6e622d7aaf18dad
mi-ael <miael.oss.1970@gmail.com>
2025-04-18 10:54:16 +0200

    fmt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<a
+  href="https://svelte.dev/docs/kit"
+  class="btn btn-primary btn-lg border-4 border-primary bg-blue-300"
+  aria-label="SvelteKit Documentation">Documentation</a
+>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
