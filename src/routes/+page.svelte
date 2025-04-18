c362d507a9c30c0038c04e22e13d99000fddbcb6
mi-ael <miael.oss.1970@gmail.com>
2025-04-18 12:21:19 +0200

    formatting, daisyui & dev dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <a
   href="https://svelte.dev/docs/kit"
-  class="btn btn-primary btn-lg border-4 border-primary bg-blue-500"
+  class="btn btn-outline btn-primary btn-lg m-2 p-2"
   aria-label="SvelteKit Documentation">Documentation</a
 >
 <p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 </p>
